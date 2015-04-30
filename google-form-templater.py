71b4f935732d1d3002082046a5d23aedf540f449
W. Trevor King <wking@tremily.us>
2015-04-29 21:26:13 -0700

    Add requests_oathlib boilerplate
    Mostly following the current [1] but with the unprefixed scopes [2].
    
    [1]: http://requests-oauthlib.readthedocs.org/en/latest/examples/google.html
    [2]: https://developers.google.com/+/api/oauth#scopes

--- google-form-templater.py
+++ google-form-templater.py
@@ -29,15 +29,25 @@ look something like:
   email = you@gmail.com
   password = your password
 
+  [client]
+  id = <the id you get from Google>.apps.googleusercontent.com
+  secret = <the secret you get from Google>
+  redirect_uri = https://your.registered/callback
+
 You can specify the location of this config file on the command line
 (with -c / --config), or use the default
-~/.config/google-form-templater.conf.
+~/.config/google-form-templater.conf.  The client credentials come
+from setting up a new web application client in the Google Developers
+Console [2].
 
 http://www.google.com/forms/about/
 https://developers.google.com/drive/
 
 [1]: https://docs.python.org/3/library/configparser.html#supported-ini-file-structure
+[2]: https://console.developers.google.com/
 """
+
+from __future__ import print_function
 
 try:  # Python 3
     import configparser
@@ -45,9 +55,59 @@ except ImportError:  # Python 2
     import ConfigParser as configparser
 import os
 
+import requests_oauthlib
+
+
+try:  # Python 2
+    input = raw_input
+except NameError:  # Python 3
+    pass
+
+
+# OAuth endpoints given in the Google API documentation
+AUTHORIZATION_BASE_URL = 'https://accounts.google.com/o/oauth2/auth'
+TOKEN_URL = 'https://accounts.google.com/o/oauth2/token'
+SCOPE = [
+    'profile',
+    'email',
+]
+
+
+def get_authorized_client(config):
+    """Get an OAuth-authorized client
+
+    Following
+    http://requests-oauthlib.readthedocs.org/en/latest/examples/google.html
+    """
+    client = requests_oauthlib.OAuth2Session(
+        client_id=config['client']['id'],
+        scope=SCOPE,
+        redirect_uri=config['client']['redirect_uri'])
+
+    # redirect user for authorization
+    authorization_url, state = client.authorization_url(
+        url=AUTHORIZATION_BASE_URL,
+        access_type='offline',    # offline for refresh token
+        approval_prompt='force')  # force to always make user click authorize
+    print('Please go here and authorize,', authorization_url)
+
+    # get the authorization verifier code from the callback url
+    redirect_response = input('Paste the full redirect URL here: ')
+
+    # fetch the access token
+    client.fetch_token(
+        token_url=TOKEN_URL,
+        client_secret=config['client']['secret'],
+        authorization_response=redirect_response)
+
+    return client
+
 
 def main(config):
-    print(config)
+    client = get_authorized_client(config)
+    # fetch a protected resource, e.g. user profile
+    r = client.get('https://www.googleapis.com/oauth2/v1/userinfo')
+    print(r.content)
 
 
 if __name__ == '__main__':
