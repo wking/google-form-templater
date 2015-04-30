730bf57375e6680f21a17df2efd803dc0d680944
W. Trevor King <wking@tremily.us>
2015-04-29 20:39:17 -0700

    Parse an INI config and pass to main() as a dict
    Since Python 3.2, ConfigParser instances have supported mapping
    protocol access (like dicts) [1], but I want both mapping protocol
    access *and* Python 2 support, so I take a few more steps to convert
    the ConfigParser instance into a dict.
    
    [1]: https://docs.python.org/3/library/configparser.html#mapping-protocol-access

--- google-form-templater.py
+++ google-form-templater.py
@@ -22,14 +22,52 @@
 
 """Create new Google Forms based on existing templates.
 
+This script is configured with a ConfigParser INI file [1], which will
+look something like:
+
+  [auth]
+  email = you@gmail.com
+  password = your password
+
+You can specify the location of this config file on the command line
+(with -c / --config), or use the default
+~/.config/google-form-templater.conf.
+
 http://www.google.com/forms/about/
 https://developers.google.com/drive/
+
+[1]: https://docs.python.org/3/library/configparser.html#supported-ini-file-structure
 """
+
+try:  # Python 3
+    import configparser
+except ImportError:  # Python 2
+    import ConfigParser as configparser
+import os
+
+
+def main(config):
+    print(config)
 
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description=__doc__.splitlines()[0])
+    parser.add_argument(
+        '-c', '--config', metavar='PATH',
+        default=os.path.expanduser(os.path.join(
+            '~', '.config', 'google-form-templater.conf')),
+        help='path to the config file')
 
     args = parser.parse_args()
+
+    cp = configparser.ConfigParser()
+    cp.read(args.config)
+    config = {}
+    for section in cp.sections():
+        config[section] = {}
+        for option in cp.options(section):
+            config[section][option] = cp.get(section, option)
+
+    main(config=config)
